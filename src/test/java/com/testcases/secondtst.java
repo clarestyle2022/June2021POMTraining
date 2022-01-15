1239adff2a604be3361ea1ccbd14235e45c419af
clarestyle2022 <clarestyle@live.com>
2022-01-15 19:05:16 +0000

    my new automation code

--- src/test/java/com/testcases/secondtst.java
+++
@@ -1,9 +0,0 @@
-package com.testcases;
-
-import org.testng.annotations.Test;
-
-public class secondtst {
-  @Test
-  public void f() {
-  }
-}
--- src/test/java/com/testcases/thirdtest.java
+++
@@ -1,9 +0,0 @@
-package com.testcases;
-
-import org.testng.annotations.Test;
-
-public class thirdtest {
-  @Test
-  public void f() {
-  }
-}
